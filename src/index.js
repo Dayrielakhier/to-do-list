e37eed882245a8d3d5f7a3197668341263cee1a9
Dayrielakhier <176357699+Dayrielakhier@users.noreply.github.com>
2025-05-23 16:33:56 +0800

    Add class and functions to manipulate the lists

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { format } from "date-fns"
+
+class ToDo {
+    constructor(title, desc, dueDate, priority) {
+        this.title = title
+        this.desc = desc
+        this.dueDate = format(new Date(dueDate), "d MMMM yyyy") 
+        this.priority = priority
+        this.completeness = "notDone"
+        this.id = crypto.randomUUID()
+    }
+
+    changeCompleteness() {
+        this.completeness = this.completeness === "notDone" ? "done" : "notDone"
+    }
+}
+
+const categories = {}
+
+function addCategory(name) {
+    categories[name] = []
+}
+
+function addTaskToList(list, task) {
+    categories[list].push(task)
+}
+
+function removeTaskFromList(list, div) {
+    categories[list].splice(categories[list].findIndex(task => task.id === div.getAttribute("data-id")), 1)
+}
+
+export {ToDo, categories, addCategory, addTaskToList, removeTaskFromList}
