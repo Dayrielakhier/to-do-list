66a7f6e0f099bf364fa41881e2a91565218f1b81
Dayrielakhier <176357699+Dayrielakhier@users.noreply.github.com>
2025-06-05 11:18:37 +0800

    Add localStorage to save data on the computer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { format } from "date-fns"
 
 class ToDo {
     constructor(title, desc, dueDate, priority) {
+        this.__type = "task"
         this.title = title
         this.desc = desc
         this.dueDate = dueDate
@@ -19,22 +20,38 @@ class ToDo {
     }
 }
 
-const categories = {}
+let categories = localStorage.getItem("todo") ? 
+JSON.parse(localStorage.getItem("todo"), reviver) : {}
+
+function reviver(key, value) {
+    if (value?.__type === "task") {
+        Object.setPrototypeOf(value, ToDo.prototype)
+    } 
+    return value
+}
+
+function updateStorage() {
+    localStorage.setItem("todo", JSON.stringify(categories))
+}
 
 function addCategory(name) {
     categories[name] = []
+    updateStorage()
 }
 
 function removeCategory(name) {
     delete categories[name]
+    updateStorage()
 }
 
 function addTaskToList(list, task) {
     categories[list].push(task)
+    updateStorage()
 }
 
 function removeTaskFromList(list, div) {
     categories[list].splice(categories[list].findIndex(task => task.id === div.getAttribute("data-id")), 1)
+    updateStorage()
 }
 
 export {ToDo, categories, addCategory, removeCategory, addTaskToList, removeTaskFromList}
